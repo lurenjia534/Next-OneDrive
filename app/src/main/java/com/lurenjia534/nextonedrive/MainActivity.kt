0386fc0230147795e3d25d4d3dcd646ee2ccdf11
luren <lurenjia534@outlook.com>
2024-08-28 18:43:32 +0800

    Project: Feat: Profile page ui

--- app/src/main/java/com/lurenjia534/nextonedrive/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/nextonedrive/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.lurenjia534.nextonedrive
 
+import DriveInfoResponse
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Send
+import androidx.compose.material.icons.filled.ArrowBack
+
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
@@ -36,6 +58,8 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -47,12 +71,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.lurenjia534.nextonedrive.ui.theme.NextOneDriveTheme
@@ -63,6 +90,7 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.lurenjia534.nextonedrive.OAuthToken.fetchAccessToken
+import com.lurenjia534.nextonedrive.Profilepage.fetchDriveInfo
 
 
 class MainActivity : ComponentActivity() {
@@ -171,28 +199,29 @@ fun MyNavigationDrawer(
                 )
             }
         },
-        content = {NavHost(
-            navController = navController,
-            startDestination = "inbox"
-        ) {
-            composable("inbox") { InboxScreen() }
-            composable("outbox") { OutboxScreen() }
-            composable("favorites") { FavoritesScreen() }
-            composable("trash") { TrashScreen() }
-            composable(
-                route = "label/{labelId}",
-                arguments = listOf(navArgument("labelId") { type = NavType.StringType })
-            ){ backStackEntry ->
-                val labelId = backStackEntry.arguments?.getString("labelId") ?: "Unknown"
-                LabelScreen(label = labelId)
-            }
+        content = {
+            NavHost(
+                navController = navController,
+                startDestination = "inbox"
+            ) {
+                composable("inbox") { InboxScreen(navController) }
+                composable("outbox") { OutboxScreen() }
+                composable("favorites") { FavoritesScreen() }
+                composable("trash") { TrashScreen() }
+                composable(
+                    route = "label/{labelId}",
+                    arguments = listOf(navArgument("labelId") { type = NavType.StringType })
+                ) { backStackEntry ->
+                    val labelId = backStackEntry.arguments?.getString("labelId") ?: "Unknown"
+                    LabelScreen(label = labelId)
+                }
             }
         }
     )
 }
 
 @Composable
-fun InboxScreen() {
+fun InboxScreen(navController: NavController) {
     // State variables for input fields
     var tenantId by remember { mutableStateOf("") }
     var clientId by remember { mutableStateOf("") }
@@ -214,12 +243,12 @@ fun InboxScreen() {
                 Snackbar(
                     snackbarData = data,
                     contentColor = MaterialTheme.colorScheme.onPrimary,
-                    containerColor =  if (showSuccessSnackbar) Color.Black else Color.Black,  // 判断是否为成功 Snackbar 来设置背景颜色
+                    containerColor = if (showSuccessSnackbar) Color.Black else Color.Black,  // 判断是否为成功 Snackbar 来设置背景颜色
                     shape = MaterialTheme.shapes.extraSmall
                 )
             }
         }
-    ) {paddingValues ->
+    ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -312,6 +341,7 @@ fun InboxScreen() {
                             successMessage = "Token: $token"
                             println("Token $token")
                             showSuccessSnackbar = true // 重置状态
+                            navController.navigate("outbox")
                         }
                     )
                 },
@@ -331,8 +361,8 @@ fun InboxScreen() {
             }
         }
         // Show success snackbar
-        if (showSuccessSnackbar){
-            LaunchedEffect(snackbarHostState){
+        if (showSuccessSnackbar) {
+            LaunchedEffect(snackbarHostState) {
                 snackbarHostState.showSnackbar(successMessage)
                 showSuccessSnackbar = false // 重置状态
                 print("Token: $successMessage")
@@ -341,30 +371,224 @@ fun InboxScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OutboxScreen() {
-    Text("Outbox Screen", modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp))
+    var driveInfo by remember { mutableStateOf<DriveInfoResponse?>(null) }
+    var errorMessage by remember { mutableStateOf("") }
+    val snackbarHostState = remember { SnackbarHostState() }
+    var showSuccessSnackbar by remember { mutableStateOf(false) }
+    var showErrorSnackbar by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    //  Fetch DriveInfo when the screen is first loaded
+    LaunchedEffect(Unit) {
+        fetchDriveInfo(
+            context = context,
+            onSuccess = { info ->
+                driveInfo = info
+                showSuccessSnackbar = true // 重置状态
+            },
+            onError = { error ->
+                errorMessage = error
+                showErrorSnackbar = true // 重置状态
+            }
+        )
+    }
+    // Handle success and error Snackbar
+    LaunchedEffect(showSuccessSnackbar, showErrorSnackbar) {
+        if (showSuccessSnackbar) {
+            snackbarHostState.showSnackbar("数据加载成功")
+            showSuccessSnackbar = false // 重置状态
+        }
+        if (showErrorSnackbar) {
+            snackbarHostState.showSnackbar("错误: $errorMessage")
+            showErrorSnackbar = false // 重置状态
+        }
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("设置") },
+                navigationIcon = {
+                    IconButton(onClick = { /* Handle back button press */ }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                },
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        content = { paddingValues ->
+            if (driveInfo != null){
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                        .padding(horizontal = 16.dp),
+                    contentPadding = PaddingValues(vertical = 16.dp)
+                ) {
+                    item {
+                        SectionTitle(title = "OneDrive 信息")
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Home,
+                            title = "名称",
+                            subtitle = driveInfo?.name ?: "N/A"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Build,
+                            title = "驱动器类型",
+                            subtitle = driveInfo?.driveType ?: "N/A"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.AccountCircle,
+                            title = "创建者",
+                            subtitle = driveInfo?.createdBy?.user?.displayName ?: "N/A"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Face,
+                            title = "最后修改者",
+                            subtitle = driveInfo?.lastModifiedBy?.user?.displayName ?: "N/A"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Email,
+                            title = "邮箱",
+                            subtitle = driveInfo?.owner?.user?.email ?: "N/A"
+                        )
+                    }
+
+                    item {
+                        Spacer(modifier = Modifier.height(16.dp))
+                        SectionTitle(title = "存储信息")
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Delete,
+                            title = "已删除存储",
+                            subtitle = "${driveInfo?.quota?.deleted?.div(1024 * 1024)} MB"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Edit,
+                            title = "剩余存储",
+                            subtitle = "${driveInfo?.quota?.remaining?.div(1024L * 1024 * 1024 * 1024)} TB"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.Info,
+                            title = "总存储空间",
+                            subtitle = "${driveInfo?.quota?.total?.div(1024L * 1024 * 1024 * 1024)} TB"
+                        )
+                    }
+                    item {
+                        SettingItem(
+                            icon = Icons.Outlined.CheckCircle,
+                            title = "已用存储空间",
+                            subtitle = "${driveInfo?.quota?.used?.div(1024L * 1024 * 1024)} GB"
+                        )
+                    }
+                }
+            }
+        }
+    )
 }
 
 @Composable
+fun SectionTitle(title: String) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.titleMedium.copy(
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold
+        ),
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .padding(horizontal = 8.dp)
+    )
+}
+
+@Composable
+fun SettingItem(icon: ImageVector, title: String, subtitle: String? = null) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+            .clickable { /* Handle item click */ },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                if (subtitle != null) {
+                    Text(
+                        text = subtitle,
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
 fun FavoritesScreen() {
-    Text("Favorites Screen", modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp))
+    Text(
+        "Favorites Screen", modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
 }
 
 @Composable
 fun TrashScreen() {
-    Text("Trash Screen", modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp))
+    Text(
+        "Trash Screen", modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
 }
 
 @Composable
 fun LabelScreen(label: String) {
-    Text("$label Screen", modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp))
+    Text(
+        "$label Screen", modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
 }
