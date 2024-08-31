2443e19e5e85389aa87b7d36f93ad0e438899774
luren <lurenjia534@outlook.com>
2024-09-01 03:34:47 +0800

    Project: Additional icon library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation ("androidx.compose.material:material-icons-extended:1.7.0-rc01")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
