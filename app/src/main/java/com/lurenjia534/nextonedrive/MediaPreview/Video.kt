67ee2427c0a55537b335a8c7f58558d2cf6442db
luren <lurenjia534@outlook.com>
2024-09-07 04:58:47 +0800

    Project: Rotating the screen does not reset the progress bar
            * save it using state

--- app/src/main/java/com/lurenjia534/nextonedrive/MediaPreview/Video.kt
+++ app/src/main/java/com/lurenjia534/nextonedrive/MediaPreview/Video.kt
@@ -9,26 +9,32 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import androidx.navigation.NavController
 @Composable
-fun VideoPreviewScreen(videoUrl: String,navController: NavController){
+fun VideoPreviewScreen(
+    videoUrl: String,
+    navController: NavController,
+){
     val context = LocalContext.current
-
+    var videoProgress by rememberSaveable { mutableLongStateOf(0L) }
     // 创建 ExoPlayer 实例
     val exoPlayer = ExoPlayer.Builder(context).build().apply {
         // 设置播放地址
         setMediaItem(MediaItem.fromUri(Uri.parse(videoUrl)))
         // 准备播放
         prepare()
+        // 恢复播放进度
+        seekTo(videoProgress)
         // 开始播放
         playWhenReady = true
     }
@@ -49,6 +55,8 @@ fun VideoPreviewScreen(videoUrl: String,navController: NavController){
     DisposableEffect(Unit) {
         // 在 Composable 销毁时释放资源
         onDispose {
+            // 保存播放进度
+            videoProgress = exoPlayer.currentPosition
             exoPlayer.release()
         }
     }
