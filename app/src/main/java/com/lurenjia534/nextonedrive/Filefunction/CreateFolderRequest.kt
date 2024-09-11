6ef0d90bbde3ebf6b51baca35b618ca61113d48a
luren <lurenjia534@outlook.com>
2024-09-12 06:45:21 +0800

    Project: Improve the data model of hierarchical folders

--- app/src/main/java/com/lurenjia534/nextonedrive/Filefunction/CreateFolderRequest.kt
+++ app/src/main/java/com/lurenjia534/nextonedrive/Filefunction/CreateFolderRequest.kt
@@ -1,9 +1,11 @@
 package com.lurenjia534.nextonedrive.Filefunction
 
 import com.google.gson.annotations.SerializedName
+import com.lurenjia534.nextonedrive.ListItem.ParentReference
 
 data class CreateFolderRequest(
     val name: String,
     val folder: Map<String,String> = emptyMap(),
-    @SerializedName("@microsoft.graph.conflictBehavior") val conflictBehavior: String = "rename"
+    @SerializedName("@microsoft.graph.conflictBehavior") val conflictBehavior: String = "rename",
+    val parentReference: ParentReference? = null  // 新增父文件夹引用
 )
